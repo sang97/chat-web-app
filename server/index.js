f438ec7d1502270525b1853290376ccbf3a43341
Sang Nguyen <sang97@vt.edu>
2019-10-13 13:59:37 -0400

    integrate socket.io and implements basic connection allow user to join and leave chat room

--- server/index.js
+++ server/index.js
@@ -10,6 +10,18 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on("connection", socket => {
+  console.log("We have a new connection");
+
+  socket.on("join", ({ name, room }) => {
+    console.log(name, room);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User had left!");
+  });
+});
+
 app.use(router);
 
 server.listen(PORT, () => console.log(`server is running on port ${PORT}`));
