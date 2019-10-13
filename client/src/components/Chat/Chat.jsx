aab10ac73374208d0eef5eb78287e01e31221f40
Sang Nguyen <sang97@vt.edu>
2019-10-13 18:38:31 -0400

    add react components, implements chat functionality

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -3,11 +3,21 @@ import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
 
+import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
+import Messages from "../Messages/Messages";
+import TextContainer from "../TextContainer/TextContainer";
+
+import "./Chat.css";
 let socket;
 
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState("");
+
   const ENDPOINT = "localhost:5000";
 
   useEffect(() => {
@@ -18,14 +28,57 @@ const Chat = ({ location }) => {
     setName(name);
     setRoom(room);
 
-    socket.emit("join", { name, room }, () => {});
+    socket.emit("join", { name, room }, error => {
+      if (error) {
+        alert(error);
+      }
+    });
+
+    return () => {
+      socket.emit("disconnect");
+      socket.disconnect();
+    };
+  }, [ENDPOINT, location.search]);
+
+  useEffect(() => {
+    socket.on("message", message => {
+      setMessages([...messages, message]);
+    });
+
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
 
     return () => {
       socket.emit("disconnect");
       socket.off();
     };
-  }, [ENDPOINT, location.search]);
-  return <h1>Chat</h1>;
+  }, [messages]);
+
+  // function for sending messages
+  const sendMessage = event => {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
+      <TextContainer users={users} />
+    </div>
+  );
 };
 
 export default Chat;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 
@@ -10,18 +11,60 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on("connection", socket => {
-  console.log("We have a new connection");
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 
-  socket.on("join", ({ name, room }) => {
-    console.log(name, room);
+app.use(router);
+app.use(cors());
+
+io.on("connection", socket => {
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+    if (error) return callback(error);
+
+    socket.join(user.room);
+
+    //let the users know they have joined a room
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the room ${user.room}`
+    });
+
+    //let existing users in the room to know that new user have joined
+    socket.broadcast.to(user.room).emit("message", {
+      user: "admin",
+      text: `${user.name}, has joined`
+    });
+
+    io.to(user.room).emit("roomData", {
+      users: getUsersInRoom(user.room)
+    });
+
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    callback();
   });
 
   socket.on("disconnect", () => {
-    console.log("User had left!");
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "admin",
+        text: `${user.name} has left the chat room!`
+      });
+
+      io.to(user.room).emit("roomData", {
+        users: getUsersInRoom(user.room)
+      });
+    }
   });
 });
 
-app.use(router);
 
 server.listen(PORT, () => console.log(`server is running on port ${PORT}`));
